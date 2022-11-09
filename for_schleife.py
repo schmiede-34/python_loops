b76aeb4f911d408b756ad2540af3fd018c561f3e
schmiede-34 <117715220+schmiede-34@users.noreply.github.com>
2022-11-09 10:51:50 +0100

    Update for_schleife.py

--- for_schleife.py
+++ for_schleife.py
@@ -2,5 +2,5 @@ for element in [2,4,6,8,12,44,456,3]:
     print(element)
     
     
-for element in "Das ist eine neue Beschreibung":
+for element in "Das ist eine neue Beschreibung-1-2-3-4":
     print(element)
