d6529b63ae7e5fbe303794760450440de87f5bec
Pierre <support@system-schmied.de>
2022-11-09 10:58:44 +0100

    3te forschleife kleine 1x1

--- for_schleife.py
+++ for_schleife.py
@@ -1,6 +1,16 @@
+"""1. For-Schleife, """
 for element in [2,4,6,8,12,44,456,3]:
     print(element)
+
+
     
-    
+"""2. For-Schleife, """    
 for element in "Das ist eine neue Beschreibung-1-2-3-4":
     print(element)
+
+
+
+"""3. For-Schleife, für das kleine 1x1 """
+for x in range(1,5):
+    for y in range(1,5):
+        print("{} * {} = {}".format(x, y, x*y))
