7dac66df36be7323ed7a5ecebe6ea95f1bc14d98
Pierre <support@system-schmied.de>
2022-11-09 11:08:38 +0100

    Kommentare hinzugefügt

--- for_schleife.py
+++ for_schleife.py
@@ -1,16 +1,16 @@
-"""1. For-Schleife, """
+"""1. For-Schleife, die Variable element wird für jeden Eintrag in der Liste[2,4..] verwendet"""
 for element in [2,4,6,8,12,44,456,3]:
     print(element)
 
 
     
-"""2. For-Schleife, """    
+"""2. For-Schleife, die Variable element wird für jedes Zeichen in dem String "Das ist.." verwendet"""    
 for element in "Das ist eine neue Beschreibung-1-2-3-4":
     print(element)
 
 
 
 """3. For-Schleife, für das kleine 1x1 """
-for x in range(1,5):
-    for y in range(1,5):
-        print("{} * {} = {}".format(x, y, x*y))
+for a in range(1,5):
+    for b in range(1,5):
+        print("{} * {} = {}".format(a, b, a*b))
